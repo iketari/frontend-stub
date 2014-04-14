bb0eedce72508ce7ad58a977693489581ec98dfa
Ivan Chashkin <i.chashkin@mail.ru>
2014-04-14 23:49:49 +0400

    Update

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var path = require('path');
 
 var app = express();
 
+// all environments
 app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'xml');
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.logger('dev'));
+}
 
-app.use(express.favicon());
-app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser("R+mP2QeS-\"WzN&<mFs]~_V6WMz X[} =<obw<G-"));
@@ -21,23 +26,35 @@ app.use(express.session({
 	}
 }));
 
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-var server = http.createServer(app);
-
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.configure('development', function(){
+	app.use(express.errorHandler());
+	app.engine('xml', require('artist').render({
+		cache: false,
+		debug: true
+	}));
+});
+app.configure('production', function(){
+	app.engine('xml', require('artist').render({
+		cache: true,
+		debug: false
+	}));
 });
 
 var scores = require('./routes/scores');
+var routes = require('./routes/index');
+
+app.get('/', routes.index);
 
 app.get('/scores', scores.getFull);
 app.get('/scores/:id', scores.getOne);
 app.post('/scores', scores.post);
 app.del('/scores/:id', scores.del);
 app.put('/scores/:id', scores.put);
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
