1d9962000133e8f9e741df4f84f38dc5a93740a6
Anton Eprev <a.eprev@gmail.com>
2014-04-15 21:18:04 +0400

    Fixed port number

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'xml');
 // development only
